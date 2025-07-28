88bffd4339dbfb779b8ec2e4d2d0e12e4402ebab
VvkSingh69 <vvksingh6939@gmail.com>
2025-07-28 22:52:41 +0530

    complete L 19

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,22 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: {
+          expiresIn: Number(configService.getOrThrow('JWT_EXPIRATION')),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
---
+++ src/auth/token-payload.interface.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+export interface TokenPayload {
+  _id: string;
+  email: string;
+}
